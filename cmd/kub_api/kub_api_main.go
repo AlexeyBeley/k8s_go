1760080c49ae0226b2fe08c60332dcf32d9dff1c
horey <alexey.beley@gmail.com>
2025-05-13 21:27:15 -0500

    debug

---
+++ cmd/kub_api/kub_api_main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"path/filepath"
+
+	"github.com/AlexeyBeley/go_common/logger"
+	"github.com/AlexeyBeley/k8s_api/kub_api"
+	"k8s.io/client-go/util/homedir"
+)
+
+var lg = &(logger.Logger{})
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	api := kub_api.KubAPI{Kubeconfig: kubeconfig}
+	api.List()
+}
